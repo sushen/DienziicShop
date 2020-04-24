e39c2c3ab64b2d2759fb386d7ec3c507c80fc651
sushen <sushenbiswasaga@gmail.com>
2020-04-24 12:14:28 +0600

    Clean

--- wp-content/plugins/elementskit-lite/libs/pro-label/views/notice.php
+++
@@ -1,4 +0,0 @@
-<div class="ekit-go-pro-notice">
-    <h2>Thank you for using ElementsKit</h2>
-    <p>To take full advantages of Elementskit and get the outstanding readymade layouts, please buy <strong>pro version</strong>.</p>
-</div>
--- wp-content/plugins/hide-header-on-posts-for-a-landing-page/views/metabox.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$post_id = $post->ID;
-wp_nonce_field($this->textname, 'custom_nonce');
-$this->render_section_fields($post_id, 'normal');
-?>
-<br/>
-
-<?php do_action('vg_page_layout/metabox/after_content', $post); ?>
